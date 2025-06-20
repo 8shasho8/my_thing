acaea538059762d510deaae85157e8d592a3516e
1F10230287 <hiroyou.0976@outlook.com>
2025-06-20 13:32:56 +0900

    科目群の追加

--- my_profile/src/app/campus/page.tsx
+++ my_profile/src/app/campus/page.tsx
@@ -13,21 +13,15 @@ export default function campusPage() {
       </header>
 
       <section className={styles.skills}>
-        <h2>～簡単な自己紹介～</h2>
+        <h2>～コンピュータ・サイエンス～</h2>
         <section className={styles.profile}>
       <img src="/profile.jpg" alt="プロフィール写真" width="500" height="300" className={styles.avatar}  />
       </section>
-        <ul>
-          <li>出身地： 新潟県新潟市</li>
-          <li>満年齢： 20歳</li>
-          <li>在籍校： 東洋大学情報連携学部情報連携学科（INIAD）</li>
-          <li>担当アイドル：　大石泉、砂塚あきら、風野灯織、月村手毬</li>
-
-        </ul>
+        <p></p>
       </section>
 
   <section className={styles.skills}>
-        <h2>～趣味～</h2>
+        <h2>～ユーザー・エクスペリエンス～</h2>
          <section className={styles.profile}>
       <img src="/profile.jpg" alt="プロフィール写真" width="500" height="300" className={styles.avatar}  />
       </section>
@@ -42,7 +36,7 @@ export default function campusPage() {
       </section>
 
         <section className={styles.skills}>
-        <h2>～資格・特技～</h2>
+        <h2>～ビジネス構築～</h2>
           <section className={styles.profile}>
       <img src="/profile.jpg" alt="プロフィール写真" width="500" height="300" className={styles.avatar}  />
       </section>
@@ -54,31 +48,17 @@ export default function campusPage() {
         </ul>
       </section>
 
-      <section className={styles.projects}>
-        <h2>経験</h2>
-
-        <article className={styles.projectCard}>
-          <h3>大学での学び</h3>
-          <p>INIADで学んだことを説明</p>
-            <Button label="詳細を見る" onClick={() => router.push('/campus')} />
-        </article>
-
-        <article className={styles.projectCard}>
-          <h3>個人的な学習</h3>
-          <p>プログラミング言語の学習</p>
-          <Button label="詳細を見る" onClick={() => alert('プロジェクト詳細ページはまだ作成中です')} />
-        </article>
-
-        <article className={styles.projectCard}>
-          <h3>経験</h3>
-          <p>レジ締め作業を自動化し、バイト先の作業時間短縮を実現しました。</p>
-          <Button label="詳細を見る" onClick={() => alert('プロジェクト詳細ページはまだ作成中です')} />
-        </article>
+        <section className={styles.skills}>
+        <h2>～データ・サイエンス～</h2>
+          <section className={styles.profile}>
+      <img src="/profile.jpg" alt="プロフィール写真" width="500" height="300" className={styles.avatar}  />
       </section>
-
-      <section className={styles.contact}>
-        <h2>お問い合わせ</h2>
-        <Button label="メールで連絡する" onClick={() => window.location.href = 'mailto:your.email@example.com'} />
+        <ul>
+          <li>普通自動車免許（MT）</li>
+          <li>基本情報技術者（勉強中）</li>
+          <li>FP（勉強中）</li>
+          <li>マリオカート</li>
+        </ul>
       </section>
     </main>
   );
